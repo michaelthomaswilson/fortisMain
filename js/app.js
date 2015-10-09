511a3d5d25ba75b0742c720e1b6183d9856c8bae
michaelthomaswilson <mwilson@pure-brand.com>
2015-10-08 20:00:03 -0600

    basic blocking completed for mobile

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+var headerHeight;
+var windowHeight;
+
+$(document).ready(function() {
+
+	headerHeight = $("#masthead").height();
+	windowHeight = $(window).innerHeight();
+	console.log(windowHeight);
+
+});
