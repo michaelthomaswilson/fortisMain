84842c8b2ee74e989fe56ef1bb1a725321ad975f
michaelthomaswilson <mwilson@pure-brand.com>
2015-10-08 19:17:30 -0600

    mobile header added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-=======
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
->>>>>>> 95753fb4111b5fb88bed25123c10b508a736545f
 $(document).foundation();
