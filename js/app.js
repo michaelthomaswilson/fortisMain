686cb341ece05f58b23c750005a98fd6bdf46da7
michaelthomaswilson <mwilson@pure-brand.com>
2015-10-23 07:45:57 -0600

    fixed scrollto nav

--- js/app.js
+++ js/app.js
@@ -3,12 +3,26 @@
 $(document).foundation();
 
 var headerHeight;
-var windowHeight;
-
+/*
 $(document).ready(function() {
 
 	headerHeight = $("#masthead").height();
 	height = $(window).innerHeight();
 	$('.fullscreen').css('height', '');
 
+});
+*/
+
+$(document).ready(function() {
+
+	headerHeight = $("#masthead").height();
+	console.log("header: " + headerHeight);
+
+  	$('a.scroll').click(function() {//$.scrollTo works EXACTLY the same way, but scrolls the whole screen
+  		var href = $(this).attr('href');
+		//$.scrollTo(href, 500,{ offset: -headerHeight, easing:'easeOutCubic' });
+		$.scrollTo($(href).offset().top, 500, {easing:'easeOutCubic'});
+
+		//return false;
+	});
 });
